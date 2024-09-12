807ac023bedf314b8f193cfaea85c0a3faae4b8d
Fensley <fortilusnehemie@gmail.com>
2024-09-12 09:57:59 -0400

    initial commit

--- closure/index.js
+++ closure/index.js
@@ -24,3 +24,41 @@ function curriedSum(a) {
 const sumFive = curriedSum(5);
 sumFive(2);
 sumFive(10);
+
+// for looop...
+
+// for arrays----
+const people = ["sam", "elex", "rene"];
+
+for (const test of people) {
+  // console.log(test)
+}
+const P = [1, 2, 3, 4, 5];
+for (const i of P) {
+  // console.log(i)
+}
+
+const items = [1, 2, 3, 4];
+for (const ite of items) {
+  if (ite % 2 === 0) {
+    console.log("even number, skip the iteration");
+    continue;
+  }
+
+  console.log("odd Number", ite);
+}
+
+const createShape = (shape) => {
+  for (const i in shape) {
+    console.log(shape[i]);
+  }
+};
+
+createShape({
+  type: "square",
+  dimensions: [10, 10],
+});
+createShape({
+  type: "rectangle",
+  dimensions: [20, 15],
+});
