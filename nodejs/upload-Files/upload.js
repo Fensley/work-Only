1d3b872798b722bec1aac85b7fd710a6b713a6fe
Fensley <fortilusnehemie@gmail.com>
2024-09-29 04:50:28 -0400

    initial commit

--- nodejs/upload-Files/upload.js
+++ nodejs/upload-Files/upload.js
@@ -1,41 +1,38 @@
-var http = require("http");
-var formidable = require("formidable");
-var fs = require("fs");
-const path = require("path");
+const fs = require("fs");
+const http = require("http");
+const formidable = require("formidable");
 
-http
-  .createServer(function (req, res) {
-    if (req.url == "/toupload") {
-      var form = new formidable.IncomingForm();
-      form.parse(req, function (err, fields, files) {
+const server = http.createServer((req, res) => {
+  if (req.url === "/toupload") {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      console.log(files);
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("error while passing the file");
+      }
+      const old = files.toupload[0].filepath;
+      const current =
+        "/Users/Fensleyrene/Desktop/" + files.toupload[0].originalFilename;
+      fs.rename(old, current, (err) => {
         if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end("nah nah");
-        }
-        console.log(files);
-        console.log(fields);
-        var oldpath = files.toupload[0].filepath;
-        var newpath =
-          "/Users/fensleyrene/Desktop/" + files.toupload[0].originalFilename;
-        fs.rename(oldpath, newpath, function (err) {
-          if (err) throw err;
-          res.write("File uploaded and moved!");
-          res.end();
-        });
-      });
-    } else {
-      fs.readFile("./form.html", (err, data) => {
-        if (err) {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write("<p>errorr</p>");
-          return res.end();
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          return res.end("error in file rename");
         }
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
+        return res.end("file has been rename successful");
       });
-    }
-  })
-  .listen(2003, () => {
-    console.log("server is running");
-  });
+    });
+  } else {
+    fs.readFile("./form.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      //   console.log(req);
+      return res.end();
+    });
+  }
+});
+server.listen(2003, (err) => {
+  if (err) throw err;
+  console.log("the server has been opened");
+});
