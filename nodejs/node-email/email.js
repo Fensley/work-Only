8d4c98805481a19084986dc87f45dff971443a47
Fensley <fortilusnehemie@gmail.com>
2024-09-30 21:09:30 -0400

    initial commit

--- nodejs/node-email/email.js
+++ nodejs/node-email/email.js
@@ -1,54 +1,35 @@
 const http = require("http");
 const nodemailer = require("nodemailer");
-
-// Create transporter object using the SMTP transport protocol
-const transporter = nodemailer.createTransport({
+const youremail = "DONTRYATHOME";
+const transpoter = nodemailer.createTransport({
   service: "gmail",
-  // You can use other services (like Outlook, Yahoo, etc.)
   auth: {
-    user: "your-email@gmail.com",
-    // Your email address
-    pass: "your-email-password",
-    // Your email password (be careful with exposing this)
+    user: youremail,
+    pass: youremail,
   },
 });
 
-// Send email function
-const sendEmail = (to, subject, text) => {
-  const mailOptions = {
-    from: "your-email@gmail.com",
-    to: to, // Receiver's email
-    subject: subject,
-    text: text,
-  };
-
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log("Error:", error);
-    } else {
-      console.log("Email sent:", info.response);
-    }
-  });
+const mailOptions = {
+  from: youremail,
+  to: youremail,
+  subject: "test 1233",
+  text: "testing two",
 };
-
-// Create a server to send email when accessed
 const server = http.createServer((req, res) => {
-  if (req.url === "/sendemail") {
-    sendEmail(
-      "recipient-email@example.com",
-      "Test Subject",
-      "Hello, this is a test email!"
-    );
+  if (req.url === "/testing") {
+    transpoter.sendMail(mailOptions, (err, info) => {
+      if (err) throw err;
+      console.log(info);
+    });
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Email sent successfully!");
+    res.end("Email is send successfully! @ fensley");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
+    res.end("Please type end end of the url to proceed");
   }
 });
 
-// Start the server
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000/");
+server.listen(2003, (err) => {
+  if (err) throw err;
+  console.log("the server went success!");
 });
