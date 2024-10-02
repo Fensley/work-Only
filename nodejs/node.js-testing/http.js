d2643216df19fe6a139b852c641e16ff15361b8d
Fensley <fortilusnehemie@gmail.com>
2024-10-02 15:39:44 -0400

    initial commit

---
+++ nodejs/node.js-testing/http.js
@@ -0,0 +1,10 @@
+// const events = require("events");
+
+// const evnt = new events.EventEmitter();
+// evnt.on("open", () => {
+//   console.log("open is one");
+// });
+// evnt.emit("open");
+
+// * upload files
+// * Email
