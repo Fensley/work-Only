ac065083bd94731bbd6d9d877e34d3b81a762627
Fensley <fortilusnehemie@gmail.com>
2024-10-03 14:22:42 -0400

    initial commit

---
+++ nodejs/node.js-testing/mail.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const nodemailer = require("nodemailer");
+const password = "ddsdddsdddnf";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "fsdsfsde@gmail.com",
+    pass: password,
+  },
+});
+
+const mailoption = {
+  from: "ffafasffdfe@gmail.com",
+  to: "zsdsdde@gmail.com",
+  subject: "yessss plan for next week",
+  Text: "yes drill baby drill",
+};
+
+transporter.sendMail(mailoption, (err) => {
+  console.log("message send");
+});
