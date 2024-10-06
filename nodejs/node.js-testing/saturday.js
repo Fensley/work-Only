e82bea39d8e01e35b1b122411410c4b34feec07a
Fensley <fortilusnehemie@gmail.com>
2024-10-06 07:50:10 -0400

    initial commit

--- nodejs/node.js-testing/saturday.js
+++ nodejs/node.js-testing/saturday.js
@@ -1,2 +1,12 @@
 const http = require("http");
 const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello world N/O</h1>");
+  return res.end();
+});
+server.listen(3000, (err) => {
+  if (err) throw err;
+  console.log("the server has been opened");
+});
