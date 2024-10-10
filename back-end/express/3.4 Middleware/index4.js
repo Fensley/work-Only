40809c0b81b015b5d38f3d062447c92220a3de6a
Fensley <fortilusnehemie@gmail.com>
2024-10-10 17:38:57 -0400

    iniital commit

--- back-end/express/3.4 Middleware/index4.js
+++ back-end/express/3.4 Middleware/index4.js
@@ -1,7 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
+// import fileURLToPath from "url";
+// import { dirname } from "path";
 
 const app = express();
 const port = 3000;
+let bandName = "";
+const path = import.meta.dirname + "/public/index.html";
+console.log(path);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passer(req, res, next) {
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+app.use(passer);
+
+app.get("/", (req, res) => {
+  res.sendFile(path);
+});
+
+app.post("/submit", (req, res) => {
+  // console.log(req.body, req.method);
+  const band = req.body.street + req.body.pet;
+  res.write("<h1>Your band generator name is:</h1>");
+  res.write(`<h3>${bandName}</h3>`);
+  return res.end();
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
