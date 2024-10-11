eb54d737356d259a9f5a455a7046e27e8fecf120
Fensley <fortilusnehemie@gmail.com>
2024-10-11 16:47:58 -0400

    initial commit

--- back-end/express/3.5 Secrets Project/index.js
+++ back-end/express/3.5 Secrets Project/index.js
@@ -1,3 +1,18 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(`To see how the final website should work, run "node solution.js".
+Make sure you have installed all the dependencies with "npm i".
+The password is ILoveProgramming`);
+});
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`server is listening on port ${port}`);
+});
