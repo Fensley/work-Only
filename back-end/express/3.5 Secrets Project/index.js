b5dc2133255a6aef4353d036eebbcb9bcda597a6
Fensley <fortilusnehemie@gmail.com>
2024-10-12 02:01:39 -0400

    initial commit

--- back-end/express/3.5 Secrets Project/index.js
+++ back-end/express/3.5 Secrets Project/index.js
@@ -1,15 +1,34 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
-
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
+const __dirnameIndex = import.meta.dirname + "/public/index.html";
+const __dirnameSecret = import.meta.dirname + "/public/secret.html";
+let pass = "";
+let __URL = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passer(req, res, next) {
+  pass = req.body["password"];
+  __URL = req.url;
+  next();
+}
+app.use(passer);
 
 app.get("/", (req, res) => {
-  res.send(`To see how the final website should work, run "node solution.js".
-Make sure you have installed all the dependencies with "npm i".
-The password is ILoveProgramming`);
+  res.sendFile(__dirnameIndex);
+});
+
+app.post("/check", (req, res) => {
+  const id = req.body["password"];
+  if (id === "ILoveProgramming") {
+    res.sendFile(__dirnameSecret);
+  } else {
+    res.sendFile(__dirnameIndex);
+  }
 });
 
 app.listen(port, (err) => {
