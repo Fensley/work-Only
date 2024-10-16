cb5401252060bf91d6022d2459e1eabdd931773d
Fensley <fortilusnehemie@gmail.com>
2024-10-16 11:35:52 -0500

    intial commit

--- back-end/express/doc-exp/airport.js
+++ back-end/express/doc-exp/airport.js
@@ -1,23 +1,32 @@
+// edit
 import express from "express";
 import bodyParser from "body-parser";
-const __filename = import.meta.dirname + "/index.html";
-console.log(__filename);
+
 const app = express();
 const port = 3000;
+const __file = import.meta.dirname + "/index.html";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let fullname = "";
+
+function paser(req, res, next) {
+  fullname = req.body["firstname"] + " " + req.body["lastname"];
+  next();
+}
+
+app.use(paser);
+
 app.get("/", (req, res) => {
-  res.sendFile(__filename);
+  res.sendFile(__file);
 });
 
 app.post("/submit", (req, res) => {
-  console.log(req.body);
-  const fullid = req.body["firstname"] + req.body["lastname"];
-  res.send(`<p>your full name is: ${fullid}  </p>`);
+  res.send(`<h1>Your name is: ${fullname}</h1>`);
 });
 
 app.listen(port, (err) => {
   if (err) throw err;
-  console.log(`server open on port ${port}`);
+  console.log("the server has been open");
 });
+console.log(fullname);
