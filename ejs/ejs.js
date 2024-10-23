ec2b3fb89d42a1f2acbd4b1222ddb4dcfbdd73e0
Fensley <fortilusnehemie@gmail.com>
2024-10-23 13:39:29 -0400

    initial commit

--- ejs/ejs.js
+++ ejs/ejs.js
@@ -1,4 +1,23 @@
-// this is the ejs
-const __dirname = import.meta.dirname;
+import express from "express";
+const app = express();
+const port = 3000;
 
-console.log(123);
+let weekday = "";
+let weekend = "";
+
+const date = new Date().getDay();
+console.log(date);
+
+if (date > 4) {
+  weekday = "yes";
+} else {
+  weekend = "no";
+}
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: weekday });
+});
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`the server is listening on port ${port} `);
+});
