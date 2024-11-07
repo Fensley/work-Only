63a192157aa6fa689973decd905219458e358fb1
Fensley <fortilusnehemie@gmail.com>
2024-11-07 11:43:42 -0500

    initial commit

--- back-end/Build-API/6.0 DIY API/index.js
+++ back-end/Build-API/6.0 DIY API/index.js
@@ -8,7 +8,7 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
-app.get("/filter", (req, res) => {
+app.get("/all", (req, res) => {
   res.json(jokes);
 });
 
