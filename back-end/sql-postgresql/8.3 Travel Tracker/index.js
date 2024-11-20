f6afe5fdf72bec04b8d0201144e3694e194537e5
Fensley <fortilusnehemie@gmail.com>
2024-11-20 14:48:20 -0500

    initial commit

--- back-end/sql-postgresql/8.3 Travel Tracker/index.js
+++ back-end/sql-postgresql/8.3 Travel Tracker/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  res.render("index.ejs");
 });
 
 app.listen(port, () => {
