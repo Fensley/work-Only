96e3727d74eda625ac234871cfaaad4d5c79f881
Fensley <fortilusnehemie@gmail.com>
2024-12-04 12:35:56 -0500

    iniital commit

--- back-end/Authentication_/9.1+Authentication+Lv.1/index.js
+++ back-end/Authentication_/9.1+Authentication+Lv.1/index.js
@@ -19,10 +19,21 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  console.log(email);
+  console.log(password);
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  console.log(email);
+  console.log(password);
+});
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+  if (err) throw err;
   console.log(`Server running on port ${port}`);
 });
