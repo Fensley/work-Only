bb39f6959f635abae19c9d25e06d83227a204ac8
Fensley <fortilusnehemie@gmail.com>
2024-12-04 14:10:57 -0500

    yay my first authentification

--- back-end/Authentication_/9.1+Authentication+Lv.1/index.js
+++ back-end/Authentication_/9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 4000;
+
+const db = new pg.Client({
+  host: "localhost",
+  user: "postgres",
+  database: "SECRET",
+  port: 3000,
+  password: "0852",
+});
+db.connect();
+// console.log(db);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,15 +33,34 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-  console.log(email);
-  console.log(password);
+  const enter = req.body;
+  // console.log(password);
+
+  const insertINTO = await db.query(
+    "INSERT INTO users VALUES($1,$2) RETURNING *",
+    [email, password]
+  );
+  const allData = await db.query("SELECT * FROM users");
+  res.render("secrets.ejs");
 });
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
-  console.log(email);
-  console.log(password);
+  const enter = req.body;
+  const allData = await db.query("SELECT * FROM users");
+
+  const exit = allData.rows.some(
+    (each) => each.email === email && each.password == password
+  );
+  if (exit) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
+  // console.log(exit);
+  // console.log(allData.rows);
+  // console.log(enter);
 });
 
 app.listen(port, (err) => {
