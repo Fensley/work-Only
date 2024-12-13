58fb083c8127e6d48b4e0475d75b9568fd5cae85
Fensley <fortilusnehemie@gmail.com>
2024-12-13 16:52:10 -0500

    initial commit

--- back-end/Authentication_/9.6+Secrets+Project/index.js
+++ back-end/Authentication_/9.6+Secrets+Project/index.js
@@ -7,9 +7,8 @@ import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
-
 const app = express();
-const port = 3000;
+const port = 4000;
 const saltRounds = 10;
 env.config();
 
@@ -165,7 +164,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "http://localhost:4000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
